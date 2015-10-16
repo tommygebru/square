bb58fbc508f1d0b3464dc4c9501524ef4507b480
JD <jt7903@gmail.com>
2015-10-15 22:31:11 -0400

    some small fixes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,65 @@
 $(function() {
 	
-	var nav = (function() {
+		
+	// (function() {
+
+
+	// 	var Nav = {
+
+	// 		init: function() {
+	// 			this.body = $(document);
+	// 			// this.header = body.find('header');
+	// 			this.button = $button = $header.find('a');
+				
+
+	// 			this.bindEvents();
+	// 		}
+
+	// 		// bindEvents: function() {
+	// 		// 	// return console.log('test');
+	// 		// }
+	// 	}
+
+	// 	Nav.init();
+
+	// })();
+	// $(document).css('background', 'red');
+
+
+var nav = (function() {
 		var $body = $(document);
 		var $header = $('header');
-		var $buttons = $header.find('a');
+		var $button = $header.find('a');
 		var $nav = $('nav');
 		var close = _closeDatShiz();
-		$buttons.on('click', _openDatShiz);
+		$button.on('click', _openDatShiz);
 		$body.on('click', _closeDatShiz);
 
 		function _openDatShiz(e) {
 			e.stopPropagation();
 			var $sibs = $(this).siblings('a').children('li');
 			
-			$buttons.toggleClass('inactive');
+			$(this).on('click', function() {
+				this._closeDatShiz;
+			});
+
+			$button.toggleClass('inactive');
 			$(this).removeClass('inactive');
 			$(this).toggleClass('active');
 			$nav.toggleClass('hide');
 			$nav.toggleClass('show');
 
 			if ($nav.hasClass('hide')) {
-				$buttons.removeClass('inactive');
+				$button.removeClass('inactive');
 			}
+			
 		}
 
 		function _closeDatShiz(e) {
 			$nav.removeClass('show');
 			$nav.addClass('hide');
-			$buttons.removeClass('active inactive');
+			$button.removeClass('active inactive');
 		}
 	})();
+
 });
